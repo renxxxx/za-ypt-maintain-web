442fe3ba8c5608cebc95829cef373ddfff4c05cf
renxinwei <413038044@qq.com>
2020-01-15 15:48:11 +0800

    1

---
+++ other/common.js
@@ -0,0 +1,76 @@
+var common = {}
+
+
+
+common.newTab = function (title, url) {
+    tabPad = $('#tabPad').length == 0 ? parent.$('#tabPad') : $('#tabPad')
+
+    tabPad.tabs('add', {
+        url: url,
+        title: title,
+        closable: true,
+        width: '100%',
+        content: `<iframe src="${url}" style="width:100%;height:100%;border:none;"/>`,
+        tools: [{
+            iconCls: 'icon-mini-refresh',
+            handler: function () {
+                var current_tab = tabPad.tabs('getSelected');
+                tabPad.tabs('update', {
+                    tab: current_tab,
+                    options: {
+                        content: current_tab.panel('options', 'content'),
+                    }
+                });
+            }
+        }
+        ]
+    });
+}
+
+
+
+common.subOmitRight = function (value, length) {
+    if (!value)
+        return ''
+    var s = ''
+    if (value.length > length)
+        s = '...'
+    return value.substring(0, length) + s
+}
+
+
+
+common.prettyFileSize = function (size) {
+    if (!size)
+        return "";
+
+    var num = 1024.00; //byte
+
+    if (size < num)
+        return size + "B";
+    if (size < Math.pow(num, 2))
+        return (size / num).toFixed(2) + "KB"; //kB
+    if (size < Math.pow(num, 3))
+        return (size / Math.pow(num, 2)).toFixed(2) + "MB"; //M
+    if (size < Math.pow(num, 4))
+        return (size / Math.pow(num, 3)).toFixed(2) + "GB"; //G
+    return (size / Math.pow(num, 4)).toFixed(2) + "TB"; //T
+}
+
+
+common.queryStringObject = function(queryString) {
+    if (!queryString)
+        queryString = window.location.search.substr(1);
+    if (queryString.startsWith('?'))
+        queryString=queryString.substr(1)
+    const queryList = queryString.split('&')
+    let result = {}
+    queryString && queryList.map((item) => {
+        let keyValue = item.split('=')
+        result[keyValue[0]] = decodeURIComponent(keyValue[1])
+    })
+    return result
+}
+
+
+
